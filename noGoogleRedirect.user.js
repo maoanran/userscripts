5c8219561fb51761d36b62533b0ab49cb4b81794
毛安然(Anran) <maoanran@gmail.com>
2017-01-14 22:42:46 +0800

    Update noGoogleRedirect.user.js

--- noGoogleRedirect.user.js
+++ noGoogleRedirect.user.js
@@ -1,11 +1,12 @@
 // ==UserScript==
 // @name           noGoogleRedirect
 // @namespace      maoanran@gmail.com
-// @description    fuck google link redirect
+// @description    fuck google search page redirection
 // @include        http://www.google.*/search*
 // @include        https://www.google.*/search*
 // @include        http://encrypted.google.*/search*
 // @include        https://encrypted.google.*/search*
+// @updateURL      https://github.com/maoanran/userscripts/raw/master/noGoogleRedirect.user.js
 // @grant          none
 // ==/UserScript==
 (function () {
